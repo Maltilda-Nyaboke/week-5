c09c165a85d2c7697d7e6af8684d905ba6674940
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-03-06 00:14:31 +0300

    bug:access value for gender

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
 let male = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 let female =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 let form = document.getElementById("form");
+
+
 displayOutput = document.getElementById("revelation");
+console.log(displayOutput);
+
 //getting user date of birth
 form.addEventListener("submit",matilda);
 
 //call the function
-
 function matilda(event) {
     event.preventDefault();
     let userDob = document.getElementById("date").value;
-    console.log(userDob);
-    // let dob = new Date(userDob);
-    // let dayDob = dob.getDay()
-    // let gender= document.getElementsByName("gender").value;
-    // for (let i in gender){
-    //     if (i.checked) {
-    //         if ( gender==="male") {
-    //         displayOutput.innerText =(`Your Akan name is ${male[dayDob]}`)
-    //         }
-    //     }
+
+    let dob = new Date(userDob);
+    let dayDob = dob.getDay()
+    let gender= document.getElementsByName("gender").value;
+    console.log(gender)
         
-    // }
+    
         
     };
