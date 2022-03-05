e673a7d9140e6281f2bafea8cb9d93d08edb974a
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-03-05 23:46:20 +0300

    style:add banner image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let male = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-let female =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
+let female =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 let form = document.getElementById("form");
 displayOutput = document.getElementById("revelation");
 //getting user date of birth
@@ -10,17 +10,17 @@ form.addEventListener("submit",matilda);
 function matilda(event) {
     event.preventDefault();
     let userDob = document.getElementById("date").value;
-    console.log(userDob)
-    let dob = new Date(userDob);
-    let dayDob = dob.getDay()
-    let gender= document.getElementsByName("gender").value;
-    for (let i in gender){
-        if (i.checked) {
-            if ( gender==="male") {
-            displayOutput.innerText =(`Your Akan name is ${male[dayDob]}`)
-            }
-        }
+    console.log(userDob);
+    // let dob = new Date(userDob);
+    // let dayDob = dob.getDay()
+    // let gender= document.getElementsByName("gender").value;
+    // for (let i in gender){
+    //     if (i.checked) {
+    //         if ( gender==="male") {
+    //         displayOutput.innerText =(`Your Akan name is ${male[dayDob]}`)
+    //         }
+    //     }
         
-    }
+    // }
         
     };
