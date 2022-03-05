b92ce5ff1ac1a0c52313e946a2cfc275e06f8886
Maltilda-Nyaboke <maltilda.bosibori.student@moringaschool.com>
2022-03-05 22:57:16 +0300

    fix:access array

--- js/script.js
+++ js/script.js
@@ -1,22 +1,23 @@
 let male = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
 let female =['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama']
 let form = document.getElementById("form");
-displayOutput = document.getElementById("revelation")
+displayOutput = document.getElementById("revelation");
 //getting user date of birth
-form.addEventListener("submit",onSubmit);
+form.addEventListener("submit",matilda);
 
 //call the function
 
-function onSubmit(event) {
-    
+function matilda(event) {
+    event.preventDefault();
     let userDob = document.getElementById("date").value;
+    console.log(userDob)
     let dob = new Date(userDob);
     let dayDob = dob.getDay()
     let gender= document.getElementsByName("gender").value;
     for (let i in gender){
         if (i.checked) {
             if ( gender==="male") {
-            
+            displayOutput.innerText =(`Your Akan name is ${male[dayDob]}`)
             }
         }
         
